07cf8bc6af4d9d3e9a1b02cfe3e918850dfc5e24
Victor <victor@mito-team.com>
2019-10-25 11:22:47 +0300

    correct

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -1,56 +1,55 @@
 @extends('layouts.app')
 
 @section('content')
+@include('layouts.mr_nav')
 
-	<div class="overlay"></div>
-	<div class="container">
-		<div class="row no-gutters slider-text js-fullheight align-items-center justify-content-center"
-		     data-scrollax-parent="true">
-			<div class="col-md-9 text-center ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
-				<h1 class="mb-3 bread" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">
-					@if (Session::has('message'))
-						{{ Session::get('message') }}
-					@else
-						{{ $message??__('mr-t.Ваши вопросы - наши ответы') }}
-					@endif
-				</h1>
-			</div>
+
+<div class="container">
+
+	<div class="row no-gutters slider-text js-fullheight align-items-center justify-content-center"
+			 data-scrollax-parent="true">
+		<div class="col-md-9 text-center ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
+			<h1 class="mb-3 bread" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">
+				@if (Session::has('message'))
+				{{ Session::get('message') }}
+				@else
+				{{ $message??__('mr-t.Ваши вопросы - наши ответы') }}
+				@endif
+			</h1>
 		</div>
 	</div>
 
-	<div class="container">
 
-		<div class="row justify-content-start pb-3">
-			<div class="col-md-7">
-				<span class="subheading">{{ __('mr-t.ЧАВо') }}</span>
-				<h3>{{ __('mr-t.Часто задаваемые вопросы') }}</h3>
-			</div>
+	<div class="row justify-content-start pb-3">
+		<div class="col-md-7">
+			<span class="subheading">{{ __('mr-t.ЧАВо') }}</span>
+			<h3>{{ __('mr-t.Часто задаваемые вопросы') }}</h3>
 		</div>
+	</div>
 
-		<div id="accordion">
-			@foreach($list as $value)
-				<div class="card-header col-md-12">
-					<a class="card-link mr-color-black" data-toggle="collapse" href="#menu{{ $value->id() }}"
-					   aria-expanded="true"
-					   aria-controls="menu{{ $value->id() }}">{{ $value->getTitle() }}<span
-							class="collapsed">
+	<div id="accordion">
+		@foreach($list as $value)
+		<div class="card-header col-md-12">
+			<a class="card-link mr-color-black" data-toggle="collapse" href="#menu{{ $value->id() }}"
+				 aria-expanded="true"
+				 aria-controls="menu{{ $value->id() }}">{{ $value->getTitle() }}<span
+					class="collapsed">
                         <i class="icon-plus-circle"></i></span><span class="expanded"><i class="icon-minus-circle"></i></span></a>
-				</div>
-
-				<div id="menu{{ $value->id() }}" class="collapse">
-					<div class="card-body">
-						{!!  $value->getText() !!}
-					</div>
-				</div>
-			@endforeach
 		</div>
+
+		<div id="menu{{ $value->id() }}" class="collapse">
+			<div class="card-body">
+				{!! $value->getText() !!}
+			</div>
+		</div>
+		@endforeach
 	</div>
 
-	<div class="container margin-t-20">
+	<div class="margin-t-20">
 		<span class="subheading">{{ __('mr-t.Обратная связь') }}</span>
 		<h3 class="form-group">{{ __('mr-t.Если остались вопросы, напишите нам') }}</h3>
 
-		{!!  \App\Http\Controllers\Helpers\MrMessageHelper::GetMessage() !!}
+		{!! \App\Http\Controllers\Helpers\MrMessageHelper::GetMessage() !!}
 		<form action="/feedback" method="post">
 			{{ Form::token() }}
 
@@ -71,4 +70,5 @@
 			<br>
 		</form>
 	</div>
+</div>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,32 +1,102 @@
 @extends('layouts.app')
 
 @section('content')
-<style>
-		/* Style the tab */
-	.tab {
-		overflow: hidden;: 1 px solid #ccc;
-	}
-
-	/* Style the buttons inside the tab */
-	.tab button {
-		background-color: inherit;
-		float: left;: none;
-		outline: none;
-		cursor: pointer;
-		padding: 14px 16px;
-		transition: 0.3s;
-		font-size: 17px;
-	}
-</style>
-
 <div class="mr-bg-img">
-	<div class="">
-		<div class="container padding-t-20">
+	<nav class="navbar navbar-expand-md navbar-light shadow-sm mr-bg-muted-blue">
+		<div class="container">
 
+			<a class="navbar-brand" href="{{ url('/') }}">
+				{{ \App\Http\Controllers\Helpers\MrBaseHelper::MR_SITE_NAME }}
+			</a>
+
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<!-- Right Side Of Navbar -->
+				<ul class="navbar-nav ml-auto">
+					<!-- Authentication Links -->
+					<li class="nav-item">
+						<a class="nav-link" href="/faq">FAQ</a>
+					</li>
+
+					@guest
+					<li class="nav-item">
+						<a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+					</li>
+					@if (Route::has('register'))
+					<li class="nav-item">
+						<a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+					</li>
+					@endif
+					@else
+					<li class="nav-item dropdown">
+						<a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+							 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+							{{ Auth::user()->name }} <span class="caret"></span>
+						</a>
+						<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+
+
+							@if(\App\Models\MrUser::me()->IsAdmin())
+							<a class="dropdown-item" href="{{ route('admin') }}">
+								Админка
+							</a>
+							<a class="dropdown-item" href="/phpmyadmin">
+								PhpMyAdmin
+							</a>
+							<a class="dropdown-item" href="{{ route('panel') }}">
+								Кабинет USER
+							</a>
+							<a class="dropdown-item" href="/clear">
+								Очистить кэш
+							</a>
+							@else
+							<a class="dropdown-item" href="{{ route('panel') }}">
+								Кабинет
+							</a>
+							@endif
+							<a class="dropdown-item" href="{{ route('logout') }}"
+								 onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+								{{ __('Logout') }}
+							</a>
+
+							<form id="logout-form" action="{{ route('logout') }}" method="POST"
+										style="display: none;">
+								@csrf
+							</form>
+						</div>
+					</li>
+					@endguest
+
+					<li class="nav-item dropdown border mr-border-radius-5">
+						<a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+							 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+							{{ mb_strtoupper(app()->getLocale()) }} <span class="caret"></span>
+						</a>
+						<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+							@foreach(\App\Models\MrLanguage::GetAll() as $item)
+							@if($item->getName() == mb_strtoupper(app()->getLocale()))
+							@continue
+							@endif
+							<a href="{{ url('/locale/'.mb_strtolower($item->getName())) }}" class="dropdown-item">
+								<i class="nav-item  fa fa-language"></i> {{ $item->getName(). ' - '. $item->getDescription() }}</a>
+							@endforeach
+						</div>
+					</li>
+				</ul>
+			</div>
+
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+							aria-controls="navbarSupportedContent" aria-expanded="false"
+							aria-label="{{ __('Toggle navigation') }}">
+				<span class="navbar-toggler-icon"></span>
+			</button>
 
 		</div>
-	</div>
+	</nav>
+	<div class="container padding-t-20">
 
+		цццццц
+	</div>
 </div>
 @endsection
 
