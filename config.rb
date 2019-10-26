a8802f57ac53bd7ce772fcecd5a5b1b45741cd1f
Allximik <allximik50@gmail.com>
2019-10-26 11:28:33 +0300

    background

--- config.rb
+++
@@ -1,8 +0,0 @@
-preeferred_syntax =:sass
-http_path = '/'
-css_dir = 'public/css'
-sass_dir = 'resources/scss'
-images_dir = 'resources/javascripts'
-relative_assets = true
-line_comments = true
-output_style = :expanded
