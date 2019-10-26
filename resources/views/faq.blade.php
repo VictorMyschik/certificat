ad24ebb7fe031d3fc4bfa932123c9439f92c92b0
Allximik <allximik50@gmail.com>
2019-10-26 11:44:40 +0300

    background

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -1,71 +1,74 @@
 @extends('layouts.app')
 
 @section('content')
-@include('layouts.mr_nav')
+  @include('layouts.mr_nav')
 
 
-<div class="container">
+  <div class="container">
 
-	<div class="row no-gutters align-items-center justify-content-center margin-t-10" data-scrollax-parent="true">
-		<h1 class="mb-3 bread">{{ __('mr-t.Ваши вопросы - наши ответы') }}</h1>
-	</div>
+    <div class="row no-gutters align-items-center justify-content-center margin-t-10" data-scrollax-parent="true">
+      <h1 class="mb-3 bread">{{ __('mr-t.Ваши вопросы - наши ответы') }}</h1>
+    </div>
 
-	<div class="row justify-content-start pb-3">
-		<div class="col-md-7">
-			<span class="subheading">{{ __('mr-t.ЧАВо') }}</span>
-			<h3>{{ __('mr-t.Часто задаваемые вопросы') }}</h3>
-		</div>
-	</div>
+    <div class="row justify-content-start pb-3">
+      <div class="col-md-7">
+        <span class="subheading">{{ __('mr-t.ЧАВо') }}</span>
+        <h3>{{ __('mr-t.Часто задаваемые вопросы') }}</h3>
+      </div>
+    </div>
 
-	<div id="accordion">
-		@foreach($list as $value)
-		<div class="card-header">
-			<a class="card-link mr-color-black mr-bold" data-toggle="collapse" href="#menu{{ $value->id() }}"
-				 aria-expanded="true"
-				 aria-controls="menu{{ $value->id() }}">{{ $value->getTitle() }}
-			</a>
-		</div>
+    <div id="accordion">
+      @foreach($list as $value)
 
-		<div id="menu{{ $value->id() }}" class="collapse">
-			<div class="card-body">
-				{!! $value->getText() !!}
-			</div>
-		</div>
-		@endforeach
-	</div>
+        <a class="card-link mr-color-black mr-bold" data-toggle="collapse" href="#menu{{ $value->id() }}"
+           aria-expanded="true"
+           aria-controls="menu{{ $value->id() }}">
+				<span class="card-header col-md-12">
+				{{ $value->getTitle() }}
+		</span>
+        </a>
 
-	<div class="margin-t-20">
-		<a data-toggle="collapse" href="#feedback" aria-expanded="true" class=" mr-color-black mr-bold">
-			{{ __('mr-t.Обратная связь') }}
-			<h3 class="form-group">{{ __('mr-t.Если остались вопросы, напишите нам') }}</h3>
-		</a>
 
-		{!! \App\Http\Controllers\Helpers\MrMessageHelper::GetMessage() !!}
+        <div id="menu{{ $value->id() }}" class="collapse">
+          <div class="card-body">
+            {!! $value->getText() !!}
+          </div>
+        </div>
+      @endforeach
+    </div>
 
-		<div id="feedback" class="collapse">
+    <div class="margin-t-20">
+      <a data-toggle="collapse" href="#feedback" aria-expanded="true" class=" mr-color-black mr-bold">
+        {{ __('mr-t.Обратная связь') }}
+        <h3 class="form-group">{{ __('mr-t.Если остались вопросы, напишите нам') }}</h3>
+      </a>
 
-			<form action="/feedback" method="post">
-				{{ Form::token() }}
+      {!! \App\Http\Controllers\Helpers\MrMessageHelper::GetMessage() !!}
 
-				<label for="name" class="form-group col-md-6 padding-l-0 padding-r-0">{{ __('mr-t.Имя') }}
-					<input required type="text" name="name" class="form-control"></label>
+      <div id="feedback" class="collapse">
 
-				<label for="email" class="form-group col-md-6 padding-l-0 padding-r-0">Email
-					<input required type="email" name="email" class="form-control"></label>
+        <form action="/feedback" method="post">
+          {{ Form::token() }}
 
-				<div class="form-group">
-					<label for="message">{{ __('mr-t.Сообщение') }}</label>
-					<textarea name="text" id="message" cols="30" rows="10" class="form-control"></textarea>
-				</div>
+          <label for="name" class="form-group col-md-6 padding-l-0 padding-r-0">{{ __('mr-t.Имя') }}
+            <input required type="text" name="name" class="form-control"></label>
 
-				<div class="form-group">
-					<input type="submit" value="{{ __('mr-t.Отправить') }}" class="btn btn-primary">
-				</div>
-				<br>
-			</form>
-		</div>
-	</div>
-</div>
+          <label for="email" class="form-group col-md-6 padding-l-0 padding-r-0">Email
+            <input required type="email" name="email" class="form-control"></label>
 
-</div>
+          <div class="form-group">
+            <label for="message">{{ __('mr-t.Сообщение') }}</label>
+            <textarea name="text" id="message" cols="30" rows="10" class="form-control"></textarea>
+          </div>
+
+          <div class="form-group">
+            <input type="submit" value="{{ __('mr-t.Отправить') }}" class="btn btn-primary">
+          </div>
+          <br>
+        </form>
+      </div>
+    </div>
+  </div>
+
+  </div>
 @endsection
