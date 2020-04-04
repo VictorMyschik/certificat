b0a216bcd8912aa3ff5212d5c0e763d4d25aabe6
Victor <victor@mito-team.com>
2020-04-04 16:39:14 +0300

    Обновление зависимостей npm

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,8 +13,7 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script type="text/javascript" defer rel="script" src="{{asset('js/app.js')}}"></script>
   <!-- Fonts -->
-  <link rel="dns-prefetch" href="//fonts.gstatic.com">
-  <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
 
   <!-- Styles -->
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
