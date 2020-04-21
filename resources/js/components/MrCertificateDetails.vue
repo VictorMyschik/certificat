e1ca651303b72a9620c94b01ef050104f996ddf7
Victor <victor@mito-team.com>
2020-04-21 17:57:49 +0300

    n/s

--- resources/js/components/MrCertificateDetails.vue
+++ resources/js/components/MrCertificateDetails.vue
@@ -1,93 +1,114 @@
 <template>
   <div class="row no-gutters col-md-12 padding-horizontal-0">
 
-    <div class="mr-btn">Сведения о документе и органе по оценке соответствия</div>
-    <a href="#" class="btn btn-success">Сведения о продукции</a>
-    <a href="#" class="mr-btn">Сведения об изготовителе</a>
-    <div class="mr-btn">Документы</div>
-
-    <div class="col-sm-12 col-md-6 mr-sm-0 p-md-2">
-      <div class="col-md-12"><h4 class="mr-auto-size-2 mr-bold">Сведения о документе</h4></div>
-      <table class="table col-md-12 mr-auto-size  table-sm">
-        <tr>
-          <td>Дата начала срока действия</td>
-          <td>12/12/2015</td>
-        </tr>
-        <tr>
-          <td>Дата окончания срока действия</td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>Эксперт - аудитор</td>
-          <td>Ивано Иван Иванович</td>
-        </tr>
-        <tr>
-          <td>Номер бланка</td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>Срок действия статуса</td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>Документ, на основании которого установлен статус</td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>Причина изменения статуса</td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>Приложения к документу</td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>Схема сертификации (декларирования)</td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>Дополнительная информация</td>
-          <td></td>
-        </tr>
-      </table>
+    <div class="row no-gutters col-md-12 m-r-5 m-l-5">
+      <div class="btn border mr_btn btn-sm m-r-5" v-on:click="change_data(1)">Общее</div>
+      <div class="btn border mr_btn btn-sm m-r-5" v-on:click="change_data(2)">Продукция</div>
+      <div class="btn border mr_btn btn-sm m-r-5" v-on:click="change_data(3)">Изготовитель</div>
+      <div class="btn border mr_btn btn-sm" v-on:click="change_data(4)">Документы</div>
     </div>
 
-    <div class="col-sm-12 col-md-6 mr-sm-0 p-md-2">
-      <div class="col-md-12"><h4 class="mr-auto-size-2 mr-bold">Сведения об органе по оценке соответствия</h4></div>
-      <table class="table col-md-12 mr-auto-size  table-sm">
-        <tr>
-          <td>Орган по сертификации</td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>ФИО руководителя органа по сертификации</td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>Телефон</td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>Электронная почта</td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>Документ, подтверждающий аккредитацию органа сертификации</td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>Дата регистрации аттестата аккредитации</td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>Место осуществления деятельности</td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>Юридический адрес</td>
-          <td></td>
-        </tr>
-      </table>
+    <div class="row no-gutters" v-if="visible_kind === 1">
+      <div class="col-sm-12 no-gutters col-md-6 mr-sm-0 padding-horizontal-0 p-md-2">
+        <div class="col-md-12"><h4 class="mr-auto-size-2 mr-bold">Сведения о документе</h4></div>
+        <table class="table col-md-12 mr-auto-size  table-sm">
+          <tr>
+            <td>Дата начала срока действия</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Дата окончания срока действия</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Эксперт - аудитор</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Номер бланка</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Срок действия статуса</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Документ, на основании которого установлен статус</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Причина изменения статуса</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Приложения к документу</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Схема сертификации (декларирования)</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Дополнительная информация</td>
+            <td></td>
+          </tr>
+        </table>
+      </div>
+      <div class="col-sm-12 no-gutters col-md-6 mr-sm-0 padding-horizontal-0 p-md-2">
+        <div class="col-md-12"><h4 class="mr-auto-size-2 mr-bold">Сведения об органе по оценке соответствия</h4></div>
+        <table class="table col-md-12 mr-auto-size table-sm">
+          <tr>
+            <td>Орган по сертификации</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>ФИО руководителя органа по сертификации</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Телефон</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Электронная почта</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Документ, подтверждающий аккредитацию органа сертификации</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Дата регистрации аттестата аккредитации</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Место осуществления деятельности</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Юридический адрес</td>
+            <td></td>
+          </tr>
+        </table>
+      </div>
+    </div>
+
+    <div class="row no-gutters" v-if="visible_kind === 2">
+      <div class="col-sm-12 no-gutters col-md-6 mr-sm-0 padding-horizontal-0 p-md-2">
+        <div class="col-md-12"><h4 class="mr-auto-size-2 mr-bold">Продукция</h4></div>
+      </div>
+    </div>
+
+    <div class="row no-gutters" v-if="visible_kind === 3">
+      <div class="col-sm-12 no-gutters col-md-6 mr-sm-0 padding-horizontal-0 p-md-2">
+        <div class="col-md-12"><h4 class="mr-auto-size-2 mr-bold">Изготовитель</h4></div>
+      </div>
+    </div>
+
+    <div class="row no-gutters" v-if="visible_kind === 4">
+      <div class="col-sm-12 no-gutters col-md-6 mr-sm-0 padding-horizontal-0 p-md-2">
+        <div class="col-md-12"><h4 class="mr-auto-size-2 mr-bold">Документы</h4></div>
+      </div>
     </div>
 
   </div>
@@ -98,18 +119,31 @@
     name: "mr-certificate-details",
     props: ['certificate'],
     data() {
-      return {}
+      return {
+        visible_kind: 1,
+      }
     },
     mounted() {
-      console.log(this.certificate)
     },
-    methods: {},
+    methods: {
+      change_data(kind) {
+        this.visible_kind = kind;
+      }
+    },
   }
 </script>
 
 <style scoped>
   td {
     padding: 0 0 0 0;
+  }
+
+  .mr_btn {
+    background-color: rgba(221, 223, 247, 0.2);
+  }
+
+  .mr_btn:hover {
+    box-shadow: 0 0 0 0.1rem rgba(221, 223, 247, 0.5);
   }
 
 </style>
--- resources/views/certificate_view.blade.php
+++ resources/views/certificate_view.blade.php
@@ -1,5 +1,5 @@
 
-<div><span class="m-r-10 mr_cert_status_{{$certificate->getStatus()}}">{{mb_strtoupper($certificate->getStatusName())}}</span>{{$certificate->getCertificateKind()->getShortName()}}</div>
+<div><span class="m-r-10 mr-bold mr_cert_status_{{$certificate->getStatus()}}">{{mb_strtoupper($certificate->getStatusName())}}</span>{{$certificate->getCertificateKind()->getShortName()}}</div>
 <div><h5 class="mr-bold" style="color: #090d2f">{{$certificate->getNumber()}}</h5></div>
 
 <div class="">
