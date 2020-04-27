d8b1b1549a35ed7d4d9dac5af1bb1e7e7df73db3
Allximik <allximik50@gmail.com>
2020-04-27 16:22:35 +0300

    shadow

--- resources/js/components/MrCertificateDetails.vue
+++ resources/js/components/MrCertificateDetails.vue
@@ -2,203 +2,227 @@
   <div class="row no-gutters col-md-12 padding-horizontal-0">
 
     <div class="row no-gutters col-md-12 m-r-5 m-l-5 m-b-10">
-      <div class="btn border mr_btn btn-sm m-r-5" v-on:click="change_data(1)">Общее</div>
-      <div class="btn border mr_btn btn-sm m-r-5" v-on:click="change_data(2)">Продукция</div>
-      <div class="btn border mr_btn btn-sm m-r-5" v-on:click="change_data(3)">Документы</div>
-      <div class="btn border mr_btn btn-sm" v-on:click="change_data(4)">Заявитель</div>
+      <div class="mr_shadow btn border mr_btn btn-sm m-r-5" v-on:click="change_data('visible_1')">Общее</div>
+      <div class="mr_shadow btn border mr_btn btn-sm m-r-5" v-on:click="change_data('visible_2')">Продукция</div>
+      <div class="mr_shadow btn border mr_btn btn-sm m-r-5" v-on:click="change_data('visible_3')">Документы</div>
+      <div class="mr_shadow btn border mr_btn btn-sm" v-on:click="change_data('visible_4')">Заявитель</div>
     </div>
 
-    <div class="row no-gutters col-md-12" v-if="visible_kind === 1">
-      <div class="col-sm-12 no-gutters col-md-6 mr-sm-0 p-md-2">
-        <div class="col-md-12 mr-auto-size-2 mt_table_header mr-bold">Сведения о документе</div>
-        <table class="table table-sm table-striped col-md-12 mr-auto-size">
-          <tr>
-            <td>Дата начала срока действия</td>
-            <td>{{certificate['DateFrom']}}</td>
-          </tr>
-          <tr>
-            <td>Дата окончания срока действия</td>
-            <td>{{certificate['DateTo']}}</td>
-          </tr>
-          <tr>
-            <td>Эксперт - аудитор</td>
-            <td>{{certificate['Auditor']}}</td>
-          </tr>
-          <tr>
-            <td>Номер бланка</td>
-            <td>{{certificate['BlankNumber']}}</td>
-          </tr>
-          <tr>
-            <td>Срок действия статуса</td>
-            <td>{{certificate['StatusDates']}}</td>
-          </tr>
-          <tr>
-            <td>Документ, на основании которого установлен статус</td>
-            <td>{{certificate['BaseDocument']}}</td>
-          </tr>
-          <tr>
-            <td>Причина изменения статуса</td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>Приложения к документу</td>
-            <td>{{certificate['WhyChange']}}</td>
-          </tr>
-          <tr>
-            <td>Схема сертификации (декларирования)</td>
-            <td>{{certificate['SchemaCertificate']}}</td>
-          </tr>
-          <tr>
-            <td>Дополнительная информация</td>
-            <td>{{certificate['Description']}}</td>
-          </tr>
-        </table>
+    <div class="row no-gutters col-md-12" v-if="visible_1">
+      <div class="shadow-sm col-sm-12 no-gutters col-md-6 mr-sm-0 p-md-2 m-b-10">
+        <h5 data-toggle="collapse" aria-expanded="false" aria-controls="base_menu_1" href="#base_menu_1"
+            class="mr_cursor mr-bold mt_table_header">Сведения о документе
+        </h5>
+        <div id="base_menu_1" class="collapse show">
+          <table class="table table-sm table-striped col-md-12 mr-auto-size">
+            <tr>
+              <td>Дата начала срока действия</td>
+              <td>{{certificate['DateFrom']}}</td>
+            </tr>
+            <tr>
+              <td>Дата окончания срока действия</td>
+              <td>{{certificate['DateTo']}}</td>
+            </tr>
+            <tr>
+              <td>Эксперт - аудитор</td>
+              <td>{{certificate['Auditor']}}</td>
+            </tr>
+            <tr>
+              <td>Номер бланка</td>
+              <td>{{certificate['BlankNumber']}}</td>
+            </tr>
+            <tr>
+              <td>Срок действия статуса</td>
+              <td>{{certificate['StatusDates']}}</td>
+            </tr>
+            <tr>
+              <td>Документ, на основании которого установлен статус</td>
+              <td>{{certificate['BaseDocument']}}</td>
+            </tr>
+            <tr>
+              <td>Причина изменения статуса</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>Приложения к документу</td>
+              <td>{{certificate['WhyChange']}}</td>
+            </tr>
+            <tr>
+              <td>Схема сертификации (декларирования)</td>
+              <td>{{certificate['SchemaCertificate']}}</td>
+            </tr>
+            <tr>
+              <td>Дополнительная информация</td>
+              <td>{{certificate['Description']}}</td>
+            </tr>
+          </table>
+        </div>
       </div>
-      <div class="col-sm-12 no-gutters col-md-6 mr-sm-0 p-md-2">
-        <div class="col-md-12 mr-auto-size-2 mt_table_header mr-bold">Орган по сертификации</div>
-        <table class="table col-md-12 mr-auto-size table-sm">
-          <tr>
-            <td colspan="2" class="mr-bold">{{authority['Name']}}</td>
-          </tr>
-          <tr>
-            <td>ФИО руководителя органа по сертификации</td>
-            <td>{{authority['FIO']}}</td>
-          </tr>
-          <tr v-if="authority['Communicate']">
-            <td colspan="2" class="mr-bold" style="padding-top: 5px;">Связь</td>
-          </tr>
-          <tr v-if="authority['Communicate']" v-for="item in authority['Communicate']">
-            <td>
-              <i class="m-r-5 mr-color-green" :class="item.icon"></i>{{item.kind}}
-            </td>
-            <td>{{item.address}}</td>
-          </tr>
-          <tr>
-            <td>Документ, подтверждающий аккредитацию органа сертификации</td>
-            <td>{{authority['DocumentNumber']}}</td>
-          </tr>
-          <tr>
-            <td>Дата регистрации аттестата аккредитации</td>
-            <td>{{authority['DocumentDate']}}</td>
-          </tr>
-          <tr>
-            <td>
-              <a v-if="authority['Address2']" target="_blank"
-                 v-bind:href="'https://yandex.ru/maps/?text=' + authority['Address2']">
-                <i class="fa mr-color-green fa-map fa-lg"></i></a> Место осуществления деятельности
-            </td>
-            <td>{{authority['Address2']}}</td>
-          </tr>
-          <tr>
-            <td>
-              <a v-if="authority['Address1']" target="_blank"
-                 v-bind:href="'https://yandex.ru/maps/?text=' + authority['Address1']">
-                <i class="fa mr-color-green fa-map fa-lg"></i></a> Юридический адрес
-            </td>
-            <td>{{authority['Address1']}}</td>
-          </tr>
-        </table>
-      </div>
-    </div>
-
-    <div class="row no-gutters col-md-12 mr-sm-0 padding-horizontal-0 p-md-2" v-if="visible_kind === 2">
-      <div class="col-md-12 mr-auto-size-2 mt_table_header mr-bold">Производитель</div>
-      <div class="mr-auto-size">
-        <div class="m-t-5 m-b-5"><i>{{manufacturer['Name']}}</i> {{manufacturer['Country']}}</div>
-
-        <table class="table col-md-12 mr-auto-size table-sm m-t-10" style="width: 100%;">
-          <tr v-if="manufacturer['Address2']">
-            <td class="mr-bold">
-              <a target="_blank" v-bind:href="'https://yandex.ru/maps/?text=' + manufacturer['Address2']">
-                <i class="fa mr-color-green fa-map fa-lg"></i></a> <span class=" p-r-10">Место осуществления деятельности:</span>
-            </td>
-            <td>{{manufacturer['Address2']}}</td>
-          </tr>
-          <tr v-if="manufacturer['Address1']">
-            <td class="mr-bold">
-              <a target="_blank" v-bind:href="'https://yandex.ru/maps/?text=' + manufacturer['Address1']">
-                <i class="fa mr-color-green fa-map fa-lg"></i></a> <span class=" p-r-10">Юридический адрес:</span>
-            </td>
-            <td>{{manufacturer['Address1']}}</td>
-          </tr>
-        </table>
+      <div class="shadow-sm col-sm-12 no-gutters col-md-6 mr-sm-0 p-md-2 m-b-10">
+        <h5 data-toggle="collapse" aria-controls="base_menu_2" href="#base_menu_2"
+            class="mr_cursor mr-bold mt_table_header">Орган по сертификации</h5>
+        <div id="base_menu_2" class="collapse show">
+          <table class="table col-md-12 mr-auto-size table-sm">
+            <tr>
+              <td colspan="2" class="mr-bold">{{authority['Name']}}</td>
+            </tr>
+            <tr>
+              <td>ФИО руководителя органа по сертификации</td>
+              <td>{{authority['FIO']}}</td>
+            </tr>
+            <tr v-if="authority['Communicate']">
+              <td colspan="2" class="mr-bold" style="padding-top: 5px;">Связь</td>
+            </tr>
+            <tr v-if="authority['Communicate']" v-for="item in authority['Communicate']">
+              <td>
+                <i class="m-r-5 mr-color-green" :class="item.icon"></i>{{item.kind}}
+              </td>
+              <td>{{item.address}}</td>
+            </tr>
+            <tr>
+              <td>Документ, подтверждающий аккредитацию органа сертификации</td>
+              <td>{{authority['DocumentNumber']}}</td>
+            </tr>
+            <tr>
+              <td>Дата регистрации аттестата аккредитации</td>
+              <td>{{authority['DocumentDate']}}</td>
+            </tr>
+            <tr>
+              <td>
+                <a v-if="authority['Address2']" target="_blank"
+                   v-bind:href="'https://yandex.ru/maps/?text=' + authority['Address2']">
+                  <i class="fa mr-color-green fa-map fa-lg"></i></a> Место осуществления деятельности
+              </td>
+              <td>{{authority['Address2']}}</td>
+            </tr>
+            <tr>
+              <td>
+                <a v-if="authority['Address1']" target="_blank"
+                   v-bind:href="'https://yandex.ru/maps/?text=' + authority['Address1']">
+                  <i class="fa mr-color-green fa-map fa-lg"></i></a> Юридический адрес
+              </td>
+              <td>{{authority['Address1']}}</td>
+            </tr>
+          </table>
+        </div>
       </div>
     </div>
 
-    <div class="row no-gutters col-md-12" v-if="visible_kind === 3">
-      <div class="col-sm-12 no-gutters mr-sm-0 padding-horizontal-0 p-md-2 mr-auto-size">
+    <div class="row no-gutters col-md-12" v-if="visible_2">
+      <div class="shadow-sm col-sm-12 no-gutters mr-sm-0 padding-horizontal-0 p-md-2 mr-auto-size m-b-10">
+        <h5 data-toggle="collapse" aria-controls="manufacturer_menu" href="#manufacturer_menu"
+            class="mr_cursor mr-bold mt_table_header">
+          Производитель</h5>
+        <div id="manufacturer_menu" class="collapse show">
+          <div class="m-t-5 m-b-5"><i>{{manufacturer['Name']}}</i> {{manufacturer['Country']}}</div>
+          <table class="table col-md-12 mr-auto-size table-sm m-t-10" style="width: 100%;">
+            <tr v-if="manufacturer['Address2']">
+              <td class="mr-bold">
+                <a target="_blank" v-bind:href="'https://yandex.ru/maps/?text=' + manufacturer['Address2']">
+                  <i class="fa mr-color-green fa-map fa-lg"></i></a> <span class=" p-r-10">Место осуществления деятельности:</span>
+              </td>
+              <td>{{manufacturer['Address2']}}</td>
+            </tr>
+            <tr v-if="manufacturer['Address1']">
+              <td class="mr-bold">
+                <a target="_blank" v-bind:href="'https://yandex.ru/maps/?text=' + manufacturer['Address1']">
+                  <i class="fa mr-color-green fa-map fa-lg"></i></a> <span class=" p-r-10">Юридический адрес:</span>
+              </td>
+              <td>{{manufacturer['Address1']}}</td>
+            </tr>
+          </table>
+        </div>
+      </div>
+    </div>
+
+    <div class="row no-gutters shadow-sm col-md-12 m-b-10" v-if="visible_3">
+      <h5 data-toggle="collapse" aria-controls="doc_menu" href="#doc_menu"
+          class="mr_cursor col-md-12 mr-bold mt_table_header">Документы, привязанные к сертификату</h5>
+      <div id="doc_menu" class="collapse shadow-sm show col-sm-12 no-gutters mr-sm-0 padding-horizontal-0 p-md-2 mr-auto-size m-b-10">
         <div v-if="documents">
           <div v-if="documents[3]">
-            <h5 data-toggle="collapse" aria-controls="menu_3" href="#menu_3" class="mr_cursor mr-bold mt_table_header">
+            <h5 data-toggle="collapse" aria-controls="doc_menu_3" href="#doc_menu_3"
+                class="mr_cursor mr-bold mt_table_header">
               {{documents[3][0]['KindName']}}</h5>
-            <table id="menu_3" class="table table-striped table-sm">
-              <thead>
-              <tr class="mr-bold">
-                <td>Документ</td>
-                <td>Дата выдачи документа</td>
-              </tr>
-              </thead>
-              <tbody>
-              <tr v-for="row in documents[3]">
-                <td>{{row['Name']}} {{row['Number']}}</td>
-                <td>{{row['Date']}}</td>
-              </tr>
-              </tbody>
-            </table>
+            <div id="doc_menu_3" class="collapse show">
+              <table class="table table-striped table-sm">
+                <thead>
+                <tr class="mr-bold">
+                  <td>Документ</td>
+                  <td>Дата выдачи документа</td>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="row in documents[3]">
+                  <td>{{row['Name']}} {{row['Number']}}</td>
+                  <td>{{row['Date']}}</td>
+                </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
           <div v-if="documents[2]">
-            <h5 data-toggle="collapse" aria-controls="menu_2" href="#menu_2" class="mr_cursor mr-bold mt_table_header">
+            <h5 data-toggle="collapse" aria-controls="doc_menu_2" href="#doc_menu_2"
+                class="mr_cursor mr-bold mt_table_header">
               {{documents[2][0]['KindName']}}</h5>
-            <table id="menu_2" class="table table-striped table-sm">
-              <thead>
-              <tr class="mr-bold">
-                <td>Документ</td>
-                <td>Дата выдачи документа</td>
-                <td>Орган выдачи</td>
-                <td>Номер и дата документа аккредитации</td>
-                <td>Примечание</td>
-              </tr>
-              </thead>
-              <tbody>
-              <tr v-for="row in documents[2]">
-                <td>{{row['Name']}} {{row['Number']}}</td>
-                <td>{{row['Date']}}</td>
-                <td>{{row['Organisation']}}</td>
-                <td>{{row['Accreditation']}}</td>
-                <td>{{row['Description']}}</td>
-              </tr>
-              </tbody>
-            </table>
+            <div id="doc_menu_2" class="collapse show">
+              <table class="table table-striped table-sm">
+                <thead>
+                <tr class="mr-bold">
+                  <td>Документ</td>
+                  <td>Дата выдачи документа</td>
+                  <td>Орган выдачи</td>
+                  <td>Номер и дата документа аккредитации</td>
+                  <td>Примечание</td>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="row in documents[2]">
+                  <td>{{row['Name']}} {{row['Number']}}</td>
+                  <td>{{row['Date']}}</td>
+                  <td>{{row['Organisation']}}</td>
+                  <td>{{row['Accreditation']}}</td>
+                  <td>{{row['Description']}}</td>
+                </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
           <div v-if="documents[1]">
-            <h5 data-toggle="collapse" aria-controls="menu_1" href="#menu_1" class="mr_cursor mr-bold mt_table_header">
+            <h5 data-toggle="collapse" aria-controls="doc_menu_1" href="#doc_menu_1"
+                class="mr_cursor mr-bold mt_table_header">
               {{documents[1][0]['KindName']}}</h5>
-            <table id="menu_1" class="table table-striped table-sm">
-              <thead>
-              <tr class="mr-bold">
-                <td>Документ</td>
-                <td>Дата выдачи документа</td>
-                <td>Признак включения документа в перечень стандартов, в результате применения которых на добровольной
-                  основе обеспечивается соблюдение установленных требований
-                </td>
-              </tr>
-              </thead>
-              <tbody>
-              <tr v-for="row_1 in documents[1]">
-                <td>{{row_1['Name']}} {{row_1['Number']}}</td>
-                <td>{{row_1['Date']}}</td>
-                <td>{{row_1['IsIncludeIn']}}</td>
-              </tr>
-              </tbody>
-            </table>
+            <div id="doc_menu_1" class="collapse show">
+              <table class="table table-striped table-sm">
+                <thead>
+                <tr class="mr-bold">
+                  <td>Документ</td>
+                  <td>Дата выдачи документа</td>
+                  <td>Признак включения документа в перечень стандартов, в результате применения которых на добровольной
+                    основе обеспечивается соблюдение установленных требований
+                  </td>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="row_1 in documents[1]">
+                  <td>{{row_1['Name']}} {{row_1['Number']}}</td>
+                  <td>{{row_1['Date']}}</td>
+                  <td>{{row_1['IsIncludeIn']}}</td>
+                </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
     </div>
 
-    <div class="row no-gutters col-md-12" v-if="visible_kind === 4">
-      <div class="col-sm-12 no-gutters mr-sm-0 padding-horizontal-0 p-md-2 mr-auto-size">
-        <div class="col-md-12 mr-auto-size-2 mt_table_header mr-bold">{{applicant['Name']}}</div>
+    <div class="row no-gutters shadow-sm col-md-12 m-b-10" v-if="visible_4">
+      <h5 data-toggle="collapse" aria-controls="applicant_base_menu" href="#applicant_base_menu"
+          class="mr_cursor col-md-12 mr-bold mt_table_header">Заявитель</h5>
+      <div id="applicant_base_menu"
+           class="collapse show col-sm-12  no-gutters mr-sm-0 padding-horizontal-0 p-md-2 mr-auto-size m-b-10">
+        <h5 class="mr-bold mt_table_header">{{applicant['Name']}}</h5>
         <table class="table col-md-12 mr-auto-size table-sm">
           <tr>
             <td>Страна</td>
@@ -245,7 +269,10 @@
     props: ['certificate_json'],
     data() {
       return {
-        visible_kind: 1,
+        visible_1: true,
+        visible_2: true,
+        visible_3: true,
+        visible_4: true,
         certificate: [],
         authority: [],
         manufacturer: [],
@@ -262,7 +289,23 @@
     },
     methods: {
       change_data(kind) {
-        this.visible_kind = kind;
+        this.visible_1 = false;
+        this.visible_2 = false;
+        this.visible_3 = false;
+        this.visible_4 = false;
+
+        if (kind === 'visible_1') {
+          this.visible_1 = true;
+          this.visible_2 = true;
+          this.visible_3 = true;
+          this.visible_4 = true;
+        } else if (kind === 'visible_2') {
+          this.visible_2 = true;
+        } else if (kind === 'visible_3') {
+          this.visible_3 = true;
+        } else if (kind === 'visible_4') {
+          this.visible_4 = true;
+        }
       }
     },
   }
@@ -294,6 +337,7 @@
     box-shadow: 0 0 0 0.1rem rgba(209, 211, 235, 0.6);
     background-color: rgba(221, 223, 247, 0.6);
   }
+
   .mr_cursor {
     cursor: pointer;
     color: #0a1041;
@@ -302,6 +346,10 @@
   .mr_cursor:hover {
     background-color: rgba(230, 232, 254, 0.3);
     color: #0a1041;
+  }
+
+  .mr_shadow {
+    box-shadow: 5px 5px rgba(221, 223, 247, 0.2);
   }
 
 </style>
